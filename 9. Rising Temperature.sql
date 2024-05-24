252b8cafbec5596cd5ee774ec1cd22819d0b0621
durgaprasadkoppala <durgarajesh.prasad.10@gmail.com>
2024-05-24 14:36:18 -0500

    File renamed

--- 9. Rising Temperature.sql
+++ 9. Rising Temperature.sql
@@ -19,7 +19,7 @@ There are no different rows with the same recordDate.
 This table contains information about the temperature on a certain day.
  
 
-Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+Write a solution to find all dates Id with higher temperatures compared to its previous dates (yesterday).
 
 Return the result table in any order.
 
